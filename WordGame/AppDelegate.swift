265a1f0a0b1229eed4c4f9547eeeda851b872d12
Oguz Parlak <oguz.parlak@ozu.edu.tr>
2022-10-02 21:12:56 +0300

    Initial commit

---
+++ WordGame/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  WordGame
+//
+//  Created by Oguz Parlak on 2.10.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    initGameScene()
+    return true
+  }
+}
+
+// MARK: - Private
+
+private extension AppDelegate {
+  
+  func initGameScene() {
+    let gameController = GameController()
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.makeKeyAndVisible()
+    window?.rootViewController = gameController
+  }
+}
---
+++ WordGame/GameController.swift
@@ -0,0 +1,30 @@
+//
+//  GameController.swift
+//  WordGame
+//
+//  Created by Oguz Parlak on 2.10.2022.
+//
+
+import UIKit
+
+public final class GameController: UIViewController {
+
+  // MARK: - Lifecycle
+  
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  // MARK: - Init
+  
+  public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+  }
+  
+  public required init?(coder: NSCoder) {
+    super.init(coder: coder)
+  }
+
+
+}
+
